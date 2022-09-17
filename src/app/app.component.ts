fee4c6dea1855aa9addfc89c33fb2b08dba86e55
Josue Guardado <jguardadosv@gmail.com>
2022-09-17 15:08:56 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {UsuarioDTO} from "./dto/usuario-DTO";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements  OnInit{ //paso 2 implements OnInit
   title = 'clienteApi';
+
+  constructor(private http:HttpClient) { // paso 3 constructor
+  }
+
+  ruta="http://localhost:3000/usuario/todos"
+  usuarios:UsuarioDTO[]=[]
+
+  ngOnInit(): void {
+
+    //get
+      this.http.get<UsuarioDTO[]>(this.ruta).subscribe(response=>{
+        this.usuarios=response
+      })
+  }
 }
+
